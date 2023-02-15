f209f881b34a07f885c12b6cc5a96bde4a2922de
alexandre <alexandresousaroberto@gmail.com>
2023-02-14 21:03:53 -0300

    ajustando quebra de linha no menssagem

--- resources/views/livewire/lovestore.blade.php
+++ resources/views/livewire/lovestore.blade.php
@@ -15,199 +15,199 @@
       <p class="text-center h1">Alguns momentos do casal</p>
         <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
       <div class="carousel-indicators hidden absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-         <button
+            <button
+               type="button"
+               data-bs-target="#carouselExampleCaptions"
+               data-bs-slide-to="0"
+               class="active"
+               aria-current="true"
+               aria-label="Slide 1"
+            ></button>
+            <button
+               type="button"
+               data-bs-target="#carouselExampleCaptions"
+               data-bs-slide-to="1"
+               aria-label="Slide 2"
+            ></button>
+            <button
+               type="button"
+               data-bs-target="#carouselExampleCaptions"
+               data-bs-slide-to="2"
+               aria-label="Slide 3"
+            ></button>
+            <button
             type="button"
             data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"
-         ></button>
-         <button
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+            ></button>
+            <button
             type="button"
             data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-         ></button>
-         <button
+            data-bs-slide-to="4"
+            aria-label="Slide 5"
+            ></button>
+            <button
             type="button"
             data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="3"
-         aria-label="Slide 4"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="4"
-         aria-label="Slide 5"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="5"
-         aria-label="Slide 6"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="6"
-         aria-label="Slide 7"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="7"
-         aria-label="Slide 8"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="8"
-         aria-label="Slide 9"
-         ></button>
-         <button
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide-to="9"
-         aria-label="Slide 10"
-         ></button>
+            data-bs-slide-to="5"
+            aria-label="Slide 6"
+            ></button>
+            <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="6"
+            aria-label="Slide 7"
+            ></button>
+            <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="7"
+            aria-label="Slide 8"
+            ></button>
+            <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="8"
+            aria-label="Slide 9"
+            ></button>
+            <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="9"
+            aria-label="Slide 10"
+            ></button>
       </div>
       <div class="carousel-inner relative w-full overflow-hidden">
          <div class="carousel-item active relative float-left w-full bg bg-info">
-            <img
-            src="{{asset ('./image/casa.jpg')}}"
-            class="block w-full h-full border border-primary border border-1 rounded "
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block  absolute text-center">
-            <h5 class="text-xl">Foto do Noivado</h5>
-            <p>Dando um passo importante para concretização de um sonho juntos</p>
-            </div>
+               <img
+               src="{{asset ('./image/casa.jpg')}}"
+               class="block w-full h-full border border-primary border border-1 rounded "
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block  absolute text-center">
+               <h5 class="text-xl">Foto do Noivado</h5>
+               <p>Dando um passo importante para concretização de um sonho juntos</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-secondary"data-bs-interval="10000">
-            <img
-            src="{{ asset('./image/avioes.jpg')}}"
-            class="block w-full h-auto border border-primary border border-1 rounded "
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Areo Porto de São Lourenço</h5>
-            <p>Primeiro compeonato Brasileiro de areo modelismo</p>
-            </div>
+               <img
+               src="{{ asset('./image/avioes.jpg')}}"
+               class="block w-full h-auto border border-primary border border-1 rounded "
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Areo Porto de São Lourenço</h5>
+               <p>Primeiro compeonato Brasileiro de areo modelismo</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-primary">
-            <img
-            src="{{ asset('./image/shopinglove.jpg')}}"
-            class="block w-full border border-primary border border-1 rounded "
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/shopinglove.jpg')}}"
+               class="block w-full border border-primary border border-1 rounded "
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-success">
-            <img
-            src="{{ asset('./image/oasis.jpg')}}"
-            class="block w-full border border-primary border border-1 rounded "
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/oasis.jpg')}}"
+               class="block w-full border border-primary border border-1 rounded "
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-success">
-            <img
-            src="{{ asset('./image/oasis2.jpg')}}"
-            class="block w-full border border-primary border border-1 rounded"
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/oasis2.jpg')}}"
+               class="block w-full border border-primary border border-1 rounded"
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-success">
-            <img
-            src="{{ asset('./image/baloes.jpg')}}"
-            class="block w-full border border-primary border border-1 rounded"
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/baloes.jpg')}}"
+               class="block w-full border border-primary border border-1 rounded"
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-primary">
-            <img
-            src="{{ asset('./image/lambari.jpg')}}"
-            class="block w-full border border-primary border border-1rounded "
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/lambari.jpg')}}"
+               class="block w-full border border-primary border border-1rounded "
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-info">
-            <img
-            src="{{ asset('./image/padaria.jpg')}}"
-            class="block w-full border border-primary border border-1 rounded"
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/padaria.jpg')}}"
+               class="block w-full border border-primary border border-1 rounded"
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-secondary">
-            <img
-            src="{{ asset('./image/inverno.jpg')}}"
-            class="block w-full border border-primary border border-1 rounded "
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/inverno.jpg')}}"
+               class="block w-full border border-primary border border-1 rounded "
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
          <div class="carousel-item relative float-left w-full bg bg-secondary">
-            <img
-            src="{{ asset('./image/amor.jpeg')}}"
-            class="block w-full border border-primary border border-1 rounded"
-            alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-            </div>
+               <img
+               src="{{ asset('./image/amor.jpeg')}}"
+               class="block w-full border border-primary border border-1 rounded"
+               alt="..."
+               />
+               <div class="carousel-caption hidden md:block absolute text-center">
+               <h5 class="text-xl">Third slide label</h5>
+               <p>Some representative placeholder content for the third slide.</p>
+               </div>
          </div>
-      </div>
-      <button
-         class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide="prev"
-      >
-         <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-         <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-         class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-         type="button"
-         data-bs-target="#carouselExampleCaptions"
-         data-bs-slide="next"
-      >
-         <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-         <span class="visually-hidden">Next</span>
-      </button>
+         </div>
+         <button
+            class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+         >
+            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+         </button>
+         <button
+            class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+         >
+            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+         </button>
       </div>
             <div class="text-center">
                <a href="{{route('message')}}"><button class="p-4 ">DEIXE UMA MENSAGEM PARA O NOIVOS</button></a>
